a41c2ffa1a65843b6abcf50156d77dc11def942c
Vladimir Ivanov <omhetehmo@yandex.ru>
2021-01-15 14:42:15 +0500

    Fetch favouirite  webcams

--- src/api.ts
+++ src/api.ts
@@ -1,15 +1,35 @@
 import axios from 'axios';
 import config from './config';
-import { ApiWebcamsResponse, Webcam } from './types';
+import { ApiWebcamsResponse } from './types';
 
 axios.defaults.baseURL = config.WEBCAM_API;
 
+const favouriteWebcams = [
+    '1495633357',
+    '1608105343',
+    '1335532071',
+    '1410334776',
+    '1239891154',
+    '1607842156',
+    '1490087893',
+    '1347204870',
+    '1515416356',
+    '1566540215',
+    '1444993841',
+    '1581179786',
+    '1332946601',
+    '1608143973',
+    '1515276672',
+];
+
 export const fetchCams = async () => {
-    const { data: { result } } = await axios.request<ApiWebcamsResponse>({
-        url: 'list/country=RU/orderby=popularity',
+    const {
+        data: { result },
+    } = await axios.request<ApiWebcamsResponse>({
+        url: `list/webcam=${favouriteWebcams.join(',')}/limit=${favouriteWebcams.length}`,
         params: {
             key: config.WEBCAM_API_KEY,
-            show:  'webcams:image,location'
+            show: 'webcams:image,location',
         },
     });
 
